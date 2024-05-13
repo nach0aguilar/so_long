c1152d8309f5b99d294cd25266b503395a65723a
nach0aguilar <nachoaguilarfdz@gmail.com>
2024-05-13 11:54:49 +0200

    Changed position of check rectangle function in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:38:32 by igaguila          #+#    #+#             */
-/*   Updated: 2024/05/13 10:37:16 by igaguila         ###   ########.fr       */
+/*   Updated: 2024/05/13 11:39:24 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,8 @@ int main (int argc, char **argv)
 		return (0);
 	check_extension(argv[1]);
 	game = pull_map(argv[1]);
-	check_walls(game);
-	ft_printf("%d\n%d\n\n", game->cols, (int)ft_strlen(game->map[0]));
 	check_rectangle(game);
+	check_walls(game);
 	int i = 0;
 	while(game->map[i])
 	{
