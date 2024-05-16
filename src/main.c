5504f6a175f556ba4f9d1913a025713be716ef6f
Ignacio Aguilar Fernandez <igaguila@c1r11s1.42malaga.com>
2024-05-16 21:40:33 +0200

    so_long OK

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 19:38:32 by igaguila          #+#    #+#             */
-/*   Updated: 2024/05/16 19:33:35 by igaguila         ###   ########.fr       */
+/*   Updated: 2024/05/16 21:40:19 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	main(int argc, char **argv)
 	t_game	*game;
 
 	if (argc == 1 || (argc == 2 && !argv[1]))
-		return (0);
+		return (ft_putstr_fd("Error: There is only one argument", 2), 0);
+	if (argc > 2)
+		return (ft_putstr_fd("Error: There are more than two arguments", 2), 0);
 	game = pull_map(argv[1]);
 	check_extension(argv[1], game);
 	check_rectangle(game);
